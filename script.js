f84bf91f667e3595b52a2a198abb3b47f3b98200
VetPlayer <lik.vk91@gmail.com>
2022-03-06 23:51:42 +0100

    add commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+$(document).ready(function() {
+  $(".header_burger").click(function(event) {
+    $('.header_burger,.header_menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-75px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-75px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
