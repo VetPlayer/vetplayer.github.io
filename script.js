8ddd0f9d81a3582144a74138b28fafef0ec1ffcc
VetPlayer <lik.vk91@gmail.com>
2022-03-14 22:26:06 +0100

    get commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $(".header_burger").click(function(event) {
+$(document).ready(function () {
+  $(".header_burger").click(function (event) {
     $('.header_burger,.header_menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
